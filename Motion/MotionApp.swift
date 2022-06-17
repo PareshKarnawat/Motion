4dd77a1aadda0004f96e036b20b76a463fd5e8a4
Paresh Karnawat <paresh.karnawat@gmail.com>
2022-06-17 08:42:25 +0530

    Initial Commit

---
+++ Motion/MotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotionApp.swift
+//  Motion
+//
+//  Created by Paresh  Karnawat on 17/06/22.
+//
+
+import SwiftUI
+
+@main
+struct MotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
